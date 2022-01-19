59bc42ead5522cd9423161f191ee6697c54f7b89
Roque Loza <rl351220@ohio.edu>
2022-01-18 19:52:26 -0500

    Initial commit

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Roque Loza
+ *        Date: January 8th, 2022
+ * Description: C++ Program
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
